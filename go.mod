1bda1841218df8e5b24e52fb62d23858eee4bba9
Allan Feldman <afeldman@newrelic.com>
2021-06-04 17:21:35 -0400

    Checkpoint.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module localhost.me/cumulative-to-delta
 
 go 1.16
+
+require go.opentelemetry.io/collector v0.27.0
--- processor/factory.go
+++ processor/factory.go
@@ -1,1 +1,9 @@
 package processor
+
+import (
+	"go.opentelemetry.io/collector/processor/processorhelper"
+)
+
+func x() {
+	processorhelper.DELETE
+}
