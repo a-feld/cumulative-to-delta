09c47a7a0d53e71f4a7866167bac63b200bfcd8e
Allan Feldman <afeldman@newrelic.com>
2021-07-20 11:39:42 -0400

    Checkpoint.

--- processor/processor.go
+++ processor/processor.go
@@ -11,7 +11,7 @@ import (
 
 type processor struct {
 	nextConsumer consumer.Metrics
-	tracker      tracking.MetricTracker
+	tracker      *tracking.MetricTracker
 }
 
 var _ component.MetricsProcessor = (*processor)(nil)
@@ -41,36 +41,32 @@ func (p processor) ConsumeMetrics(ctx context.Context, md pdata.Metrics) error {
 			ms := ilm.Metrics()
 			for k := 0; k < ms.Len(); k++ {
 				m := ms.At(k)
+				baseIdentity := tracking.MetricIdentity{
+					Resource:               rm.Resource(),
+					InstrumentationLibrary: ilm.InstrumentationLibrary(),
+					MetricDataType:         m.DataType(),
+					MetricName:             m.Name(),
+					MetricDescription:      m.Description(),
+					MetricUnit:             m.Unit(),
+				}
 				switch m.DataType() {
-				case pdata.MetricDataTypeSum:
-					ms := m.Sum()
-					if ms.AggregationTemporality() == pdata.AggregationTemporalityCumulative {
-						ms.SetAggregationTemporality(pdata.AggregationTemporalityDelta)
-
-						dps := ms.DataPoints()
-						for d := 0; d < dps.Len(); d++ {
-							dp := dps.At(d)
-							tdp := tracking.DataPoint{
-								Identity: tracking.MetricIdentity{
-									Resource:               rm.Resource(),
-									InstrumentationLibrary: ilm.InstrumentationLibrary(),
-									MetricDataType:         m.DataType(),
-									MetricIsMonotonic:      ms.IsMonotonic(),
-									MetricName:             m.Name(),
-									MetricDescription:      m.Description(),
-									MetricUnit:             m.Unit(),
-									LabelsMap:              dp.LabelsMap(),
-								},
-								Point: tracking.MetricPoint{
-									ObservedTimestamp: dp.Timestamp(),
-									Value:             dp.Value(),
-								},
-							}
-							delta := p.tracker.Convert(tdp)
-							dp.SetStartTimestamp(delta.StartTimestamp)
-							dp.SetValue(delta.Value.(float64))
-						}
+				case pdata.MetricDataTypeIntSum:
+					ms := m.IntSum()
+					if ms.AggregationTemporality() != pdata.AggregationTemporalityCumulative {
+						break
 					}
+					ms.SetAggregationTemporality(pdata.AggregationTemporalityDelta)
+					baseIdentity.MetricIsMonotonic = ms.IsMonotonic()
+					p.convertDataPoints(ms.DataPoints(), baseIdentity)
+				case pdata.MetricDataTypeSum:
+
+					ms := m.Sum()
+					if ms.AggregationTemporality() != pdata.AggregationTemporalityCumulative {
+						break
+					}
+					ms.SetAggregationTemporality(pdata.AggregationTemporalityDelta)
+					baseIdentity.MetricIsMonotonic = ms.IsMonotonic()
+					p.convertDataPoints(ms.DataPoints(), baseIdentity)
 				}
 			}
 		}
@@ -78,10 +74,47 @@ func (p processor) ConsumeMetrics(ctx context.Context, md pdata.Metrics) error {
 	return p.nextConsumer.ConsumeMetrics(ctx, md)
 }
 
+func (p processor) convertDataPoints(in interface{}, baseIdentity tracking.MetricIdentity) {
+	switch dps := in.(type) {
+	case pdata.DoubleDataPointSlice:
+		for i := 0; i < dps.Len(); i++ {
+			dp := dps.At(i)
+			id := baseIdentity
+			id.LabelsMap = dp.LabelsMap()
+			point := tracking.MetricPoint{
+				ObservedTimestamp: dp.Timestamp(),
+				Value:             dp.Value(),
+			}
+			delta := p.tracker.Convert(tracking.DataPoint{
+				Identity: id,
+				Point:    point,
+			})
+			dp.SetStartTimestamp(delta.StartTimestamp)
+			dp.SetValue(delta.Value.(float64))
+		}
+	case pdata.IntDataPointSlice:
+		for i := 0; i < dps.Len(); i++ {
+			dp := dps.At(i)
+			id := baseIdentity
+			id.LabelsMap = dp.LabelsMap()
+			point := tracking.MetricPoint{
+				ObservedTimestamp: dp.Timestamp(),
+				Value:             dp.Value(),
+			}
+			delta := p.tracker.Convert(tracking.DataPoint{
+				Identity: id,
+				Point:    point,
+			})
+			dp.SetStartTimestamp(delta.StartTimestamp)
+			dp.SetValue(delta.Value.(int64))
+		}
+	}
+}
+
 func createProcessor(_ *Config, nextConsumer consumer.Metrics) (*processor, error) {
 	p := &processor{
 		nextConsumer: nextConsumer,
-		tracker:      tracking.MetricTracker{},
+		tracker:      &tracking.MetricTracker{},
 	}
 	return p, nil
 }
