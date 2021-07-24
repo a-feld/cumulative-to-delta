e36734e95f2451f6da1cc6a6f4bad17a690af610
Allan Feldman <afeldman@newrelic.com>
2021-07-24 02:42:46 -0400

    Add benchmark.

---
+++ processor_test.go
@@ -0,0 +1,48 @@
+package cumulativetodeltaprocessor
+
+import (
+	"context"
+	"testing"
+
+	"go.opentelemetry.io/collector/consumer/consumertest"
+	"go.opentelemetry.io/collector/model/pdata"
+)
+
+func BenchmarkConsumeMetrics(b *testing.B) {
+	c := consumertest.NewNop()
+	cfg := createDefaultConfig()
+	p, err := createProcessor(cfg.(*Config), c)
+	if err != nil {
+		panic(err)
+	}
+
+	metrics := pdata.NewMetrics()
+	rms := metrics.ResourceMetrics().AppendEmpty()
+	r := rms.Resource()
+	r.Attributes().Insert("resource", pdata.NewAttributeValueBool(true))
+	ilms := rms.InstrumentationLibraryMetrics().AppendEmpty()
+	ilms.InstrumentationLibrary().SetName("test")
+	ilms.InstrumentationLibrary().SetVersion("0.1")
+	m := ilms.Metrics().AppendEmpty()
+	m.SetDataType(pdata.MetricDataTypeSum)
+	m.Sum().SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
+	m.Sum().SetIsMonotonic(true)
+	dp := m.Sum().DataPoints().AppendEmpty()
+	dp.LabelsMap().Insert("tag", "value")
+	dp.SetValue(100.0)
+
+	reset := func() {
+		m.Sum().SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
+		dp.SetValue(100.0)
+	}
+
+	// Load initial value
+	p.ConsumeMetrics(context.Background(), metrics)
+	reset()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.ConsumeMetrics(context.Background(), metrics)
+		reset()
+	}
+}
